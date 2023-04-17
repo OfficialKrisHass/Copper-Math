95caad747f1e75218720b528eeb3445082630368
OfficialChrisHusky <81988470+OfficialChrisHusky@users.noreply.github.com>
2023-04-17 19:57:29 +0200

    Min Max functions

--- CMath/CMath.h
+++ CMath/CMath.h
@@ -1,3 +1,18 @@
 #pragma once
 
 #include "Trigonometry.h"
+
+#define Min(type) type Min(type a, type b) { return (a > b) ? b : a; }
+#define Max(type) type Max(type a, type b) { return (a > b) ? a : b; }
+
+namespace CMath {
+
+	Min(int)
+	Min(float)
+	Min(double)
+
+	Max(int)
+	Max(float)
+	Max(double)
+
+}
