b10143a5c825fd8312a2f190751389a6bb9d3604
OfficialChrisHusky <81988470+OfficialChrisHusky@users.noreply.github.com>
2023-04-18 15:30:13 +0200

    Random number generators

--- CMath/App.cpp
+++ CMath/App.cpp
@@ -2,11 +2,32 @@
 
 #include "CMath.h"
 
+using namespace CMath;
+
 int main() {
 
-	std::cout << "Hello, World!" << std::endl;
-	std::cin.get();
 
+	uint32_t nMin = 0;
+	uint32_t nMax = 0;
+	uint32_t nWrong = 0;
+	uint32_t timeStart = (uint32_t) time(nullptr);
+	for (int i = 0; i < 1'000'000'000; i++) {
+
+		int random = RandomInt(0, 100);
+		if (random == 100) nMax++;
+		if (random == 0) nMin++;
+		if (random > 100 || random < 0) nWrong++;
+
+	}
+	uint32_t timeElapsed = (uint32_t) time(nullptr) - timeStart;
+
+	std::cout << "1 000 000 000 runs done.\nTime Elapsed: " << timeElapsed << std::endl << std::endl;
+
+	std::cout << "nMin: " << nMin << std::endl;
+	std::cout << "nMax: " << nMax << std::endl;
+	std::cout << "nWrong: " << nWrong << std::endl;
+
+	std::cin.get();
 	return 0;
 
 }
--- CMath/CMath.h
+++ CMath/CMath.h
@@ -2,17 +2,36 @@
 
 #include "Trigonometry.h"
 
-#define Min(type) type Min(type a, type b) { return (a > b) ? b : a; }
-#define Max(type) type Max(type a, type b) { return (a > b) ? a : b; }
+#include <stdint.h>
+
+#define MinImpl(type) inline type Min(type a, type b) { return (a > b) ? b : a; }
+#define MaxImpl(type) inline type Max(type a, type b) { return (a > b) ? a : b; }
 
 namespace CMath {
 
-	Min(int)
-	Min(float)
-	Min(double)
+	MinImpl(int)
+	MinImpl(float)
+	MinImpl(double)
 
-	Max(int)
-	Max(float)
-	Max(double)
+	MaxImpl(int)
+	MaxImpl(float)
+	MaxImpl(double)
+
+	inline int32_t RoundNearest(float num) { return (int32_t) roundf(num); }
+	inline int32_t RoundNearest(double num) { return (int32_t) round(num); }
+
+	inline int32_t RoundSmallest(float num) { return (int32_t) num; }
+	inline int32_t RoundSmallest(double num) { return (int32_t) num; }
+
+	inline void SetRandomSeed(uint32_t seed = 2'147'483'647) {
+
+		if (seed == 2'147'483'647) seed = time(nullptr);
+		srand(seed);
+
+	}
+
+	inline int RandomInt(int min = 0, int max = 10) { return min + (rand() % (max - min + 1)); }
+	inline float RandomFloat(float min = 0.0f, float max = 1.0f) { return min + (float) rand() / ((float) RAND_MAX / max); }
+	inline double RandomDouble(double min = 0.0f, double max = 1.0f) { return min + (double) rand() / (RAND_MAX / max); }
 
 }
